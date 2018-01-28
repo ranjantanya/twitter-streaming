33c7e79a21a5c66032f2b3e906ef48fa457122b7
Ranjan <TR035748@cerner.net>
2018-01-28 20:32:58 +0000

    added separate thread to handle report generation

--- tweets.py
+++ tweets.py
@@ -1,25 +1,29 @@
 import json
+import re
 import threading
 import time
+
+from stop_words import get_stop_words
+from textblob import TextBlob
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
-from stop_words import get_stop_words
-from textblob import TextBlob
-import re
+
 import local_config as config
 
 stop_words = get_stop_words('english')
 stop_words.append(get_stop_words('en'))
 
-count_of_user_tweets = dict()
-count_of_unique_words = dict()
-count_of_each_domain_url = dict()
+user_to_tweet_count_map = dict()
+word_to_count_map = dict()
+domain_name_to_count_map = dict()
 total_count_of_urls = 0
 tweet_list = []
 rate_limit_reached = False
-start_time = None
 FIVE_MINUTES_IN_MS = 300000
+WAIT_TIME_BEFORE_NEXT_PROCESSING = 60
+DOMAIN_NAME_PATTERN = '\/\/([a-zA-Z0-9.-]*)[\/\?]?'
+start_time = None
 
 
 class TweetReporter(threading.Thread):
@@ -35,12 +39,13 @@ class TweetReporter(threading.Thread):
     def run(self):
         global start_time, tweet_list, rate_limit_reached
         current_millis = int(round(time.time() * 1000))
+
+        # if more than 5 mins have elapsed then start deleting tweets which are older
+        # than 5 mins. This way we will maintain a window of latest tweets only (last 5 mins only)
         if current_millis - FIVE_MINUTES_IN_MS > start_time:
-            # if more than 5 mins have elapsed then start deleting tweets which are older
-            # than 5 mins. This way we will maintain a window of latest tweets only (last 5 mins only)
             self.delete_old_tweets(current_millis)
 
-        # generate reports based on last five mins data
+        print('########################################## Report Start ##########################################')
         if len(tweet_list) > 0:
             self.preprocess_tweets(current_millis)
             self.generate_user_report()
@@ -48,20 +53,21 @@ class TweetReporter(threading.Thread):
             self.generate_links_report()
         else:
             if rate_limit_reached:
-                print("Rate limit has reached. Deferring report generation.")
+                print('Rate limit has reached. Deferring report generation.')
             else:
-                print("No tweets found")
+                print('No tweets found')
+        print('########################################## Report End ##########################################')
 
     def delete_old_tweets(self, current_millis):
         """
         Deletes tweets which are older than 5 mins
 
-        :param current_millis: timestamp when current instance of report generation started
+        :param current_millis: timestamp when current report generation started
         :return:
         """
         global tweet_list
-        print("\nTweet list length before deleting old tweets {}".format(str(len(tweet_list))))
-        first_min_end_ts = current_millis - 15000  # current time - 5 mins
+        print('\nTweet list length before deleting old tweets {}'.format(str(len(tweet_list))))
+        first_min_end_ts = current_millis - FIVE_MINUTES_IN_MS  # current time - 5 mins
         # delete the 1st minute data
         index = 0
         for tweet in tweet_list:
@@ -73,34 +79,39 @@ class TweetReporter(threading.Thread):
 
         # slice tweet list to remove older tweets
         tweet_list = tweet_list[index:]
-        print("Number of tweets deleted {}".format(str(index + 1)))
-        print("Tweet list length after deleting old tweets {}".format(str(len(tweet_list))))
+        print('Number of tweets deleted {}'.format(str(index + 1)))
+        print('Tweet list length after deleting old tweets {}'.format(str(len(tweet_list))))
 
     def preprocess_tweets(self, current_millis):
         """
         Preprocess tweets, within the last 5 mins, to populate global dictionaries with
         details of users, words, links which will be used for report generation
 
-        :param current_millis: timestamp when current instance of report generation started
+        :param current_millis: timestamp when the report generation started
         :return:
         """
-        global tweet_list, count_of_user_tweets, count_of_unique_words, count_of_each_domain_url, total_count_of_urls
+        global tweet_list, user_to_tweet_count_map, word_to_count_map, domain_name_to_count_map, total_count_of_urls
         # clear global dictionaries to start afresh
-        count_of_each_domain_url.clear()
-        count_of_user_tweets.clear()
-        count_of_unique_words.clear()
+        domain_name_to_count_map.clear()
+        user_to_tweet_count_map.clear()
+        word_to_count_map.clear()
         total_count_of_urls = 0
 
-        # filter tweets to fetch the latest tweets (within last 5 mins)
+        # filter to fetch the latest tweets (within last 5 mins)
+        # needed because new tweets might get added while we're processing
         tweets_in_last_5_min_window = []
         for tweet in tweet_list:
             try:
                 tweet_timestamp = int(tweet['timestamp_ms'])
+                # ignore older tweets (more than 5 mins old)
+                # ignore tweets which are newer than when the report generation started
+                # ignore retweets
                 if (current_millis - FIVE_MINUTES_IN_MS) <= tweet_timestamp <= current_millis \
                         and 'retweeted_status' not in tweet.keys():
                     tweets_in_last_5_min_window.append(tweet)
-            except:
-                print('Error while processing tweet: ' + str(tweet))
+            except Exception as e:
+                print('Error while processing tweet: {}'.format(str(tweet)))
+                print('Exception {}'.format(e))
 
         # process relevant tweets and fill up global dictionaries
         for tweet in tweets_in_last_5_min_window:
@@ -108,27 +119,27 @@ class TweetReporter(threading.Thread):
                 # process users
                 user_id = tweet['user']['id']
                 user_name = tweet['user']['name']
-                count_user_tweets = count_of_user_tweets.get((user_id, user_name), 0) + 1
-                count_of_user_tweets[(user_id, user_name)] = count_user_tweets
+                count_user_tweets = user_to_tweet_count_map.get((user_id, user_name), 0) + 1
+                user_to_tweet_count_map[(user_id, user_name)] = count_user_tweets
 
                 # process urls
                 if 'extended_tweet' in tweet.keys():
                     # text contains truncated tweet, so pick from extended_tweet
-                    tweet_text = tweet['extended_tweet']['full_text']
                     list_of_urls = tweet['extended_tweet']['entities']['urls']
+                    tweet_text = tweet['extended_tweet']['full_text']
                 else:
-                    tweet_text = tweet['text']
                     list_of_urls = tweet['entities']['urls']
+                    tweet_text = tweet['text']
 
-                if len(list_of_urls) != 0:
+                if list_of_urls is not None and len(list_of_urls) != 0:
                     for url_in_tweet in list_of_urls:
                         total_count_of_urls += 1
                         expanded_url = url_in_tweet['expanded_url']
-                        domain = re.search('\/\/([a-zA-Z0-9.-]*)[\/\?]?', expanded_url)
+                        domain = re.search(DOMAIN_NAME_PATTERN, expanded_url)
                         if domain:
                             domain_name = domain.group(1)
-                            url_count = count_of_each_domain_url.get(domain_name, 0) + 1
-                            count_of_each_domain_url[domain_name] = url_count
+                            url_count = domain_name_to_count_map.get(domain_name, 0) + 1
+                            domain_name_to_count_map[domain_name] = url_count
 
                 # process words
                 tweet_text = re.sub(r'@\w+', '', tweet_text)  # remove @ mentions
@@ -136,41 +147,58 @@ class TweetReporter(threading.Thread):
 
                 for word in tweet_blob.words:
                     if word not in stop_words and word != "'s" and len(word) > 1:
-                        count_of_word = count_of_unique_words.get(word, 0) + 1
-                        count_of_unique_words[word] = count_of_word
+                        count_of_word = word_to_count_map.get(word, 0) + 1
+                        word_to_count_map[word] = count_of_word
 
-            except:
-                print('Error in tweet: ' + str(tweet))
+            except Exception as e:
+                print('Error in tweet: {}'.format(str(tweet)))
+                print('Exception {}'.format(e))
 
     def generate_user_report(self):
-        global count_of_user_tweets
-        if count_of_user_tweets is None or len(count_of_user_tweets) == 0:
-            print('zero users have tweeted with the given keyword')
+        """
+        Prints the names of all Twitter users who tweeted with given keyword along with the count of tweets from them.
+        """
+        print('************** User Report **************')
+        global user_to_tweet_count_map
+        if user_to_tweet_count_map is None or len(user_to_tweet_count_map) == 0:
+            print('Zero users have tweeted with the given keyword')
         else:
-            for (user_id, user_name) in count_of_user_tweets:
+            for (user_id, user_name) in user_to_tweet_count_map:
                 print('{user} has posted {count} tweets'.format(user=user_name,
-                                                                count=count_of_user_tweets[(user_id, user_name)]))
+                                                                count=user_to_tweet_count_map[(user_id, user_name)]))
 
     def generate_links_report(self):
-        global count_of_each_domain_url, total_count_of_urls
-        if count_of_each_domain_url is None or len(count_of_each_domain_url) == 0:
-            print('zero urls till now')
+        """"
+        Prints the total number of links included in the tweets followed by a list of the unique domains sorted
+        by the count. Any shortened links are expanded
+        """
+        print('************** Links Report **************')
+        global domain_name_to_count_map, total_count_of_urls
+
+        print('Total number of links in the tweets = {}'.format(total_count_of_urls))
+        if domain_name_to_count_map is None or len(domain_name_to_count_map) == 0:
+            print('Zero urls till now')
         else:
             print('List of unique domains in decreasing frequency:')
             sorted_domain_with_frequency = [k for k in
-                                            sorted(count_of_each_domain_url, key=count_of_each_domain_url.get,
+                                            sorted(domain_name_to_count_map, key=domain_name_to_count_map.get,
                                                    reverse=True)]
             for domain in sorted_domain_with_frequency:
-                print('{domain} :   {count}'.format(domain=domain, count=count_of_each_domain_url[domain]))
+                print('{domain}    :   {count}'.format(domain=domain, count=domain_name_to_count_map[domain]))
 
     def generate_content_report(self):
-        global count_of_unique_words
-        if count_of_unique_words is None or len(count_of_unique_words) == 0:
-            print('zero words till now')
+        """
+        Prints the number of unique words used in the tweets followed by the list of top 10 words sorted by occurence.
+        Removes the common function words like a, an, the, of, with etc.
+        """
+        print('************** Content Report **************')
+        global word_to_count_map
+        if word_to_count_map is None or len(word_to_count_map) == 0:
+            print('Zero words till now')
 
         else:
-            print('No. of unique words till now: {count}'.format(count=len(count_of_unique_words.keys())))
-            sorted_word_count = [k for k in sorted(count_of_unique_words, key=count_of_unique_words.get, reverse=True)]
+            print('No. of unique words till now: {count}'.format(count=len(word_to_count_map.keys())))
+            sorted_word_count = [k for k in sorted(word_to_count_map, key=word_to_count_map.get, reverse=True)]
             print('The most frequently used 10 words are:')
             for i in range(0, 10):
                 # pass
@@ -190,11 +218,10 @@ class TweetTimer(threading.Thread):
         self.name = name
 
     def run(self):
-        global start_time
-        start_time = int(round(time.time() * 1000))
         while True:
-            # wait for 60 seconds before generating the report
-            time.sleep(5)
+            # wait before generating the report
+            print("Sleeping for 1 minute")
+            time.sleep(WAIT_TIME_BEFORE_NEXT_PROCESSING)
             # generate report in a new thread
             TweetReporter(2, "Tweet Reporter").start()
 
@@ -205,12 +232,26 @@ class TwitterStreamListener(StreamListener):
     """
 
     def on_data(self, data):
+        """
+        Called when raw data is received from the connection
+
+        :param data: raw tweet data received
+        :return:
+        """
         global tweet_list
-        json_data = json.loads(data)
-        tweet_list.append(json_data)
+        tweet_json = json.loads(data)
+        # adding only valid tweets
+        if 'timestamp_ms' in tweet_json:
+            tweet_list.append(tweet_json)
         return True
 
     def on_error(self, status):
+        """
+        Called when a non-200 status code is returned
+
+        :param status: response status code
+        :return:
+        """
         global rate_limit_reached
         if status == 420:
             rate_limit_reached = True
@@ -218,9 +259,16 @@ class TwitterStreamListener(StreamListener):
         return True
 
     def on_connect(self):
+        """"
+        Called once connected to streaming server.
+
+        This will be invoked once a successful response
+        is received from the server. Allows the listener
+        to perform some work prior to entering the read loop.
+        """
         global rate_limit_reached
         rate_limit_reached = False
-        print("Connection Established")
+        print('Connection Established')
 
 
 if __name__ == "__main__":
@@ -238,14 +286,19 @@ if __name__ == "__main__":
     # ask for keyword to track
     keyword = None
     while keyword is None:
-        keyword = input('Enter the keyword: ')
-        if keyword == ' ' or keyword == '':
+        keyword = input('Enter the keyword to track: ')
+        keyword = keyword.strip()
+        if keyword == '':
+            print('Please enter a valid keyword\n')
             keyword = None
 
     # initialize stream listener and start accepting tweets via twitter streams API
     stream_listener = TwitterStreamListener()
     stream = Stream(auth, stream_listener)
     stream.filter(track=[keyword], async=True)
+
+    # track start time. Useful for processing the first 5 minutes of data
+    start_time = int(round(time.time() * 1000))
 
     # start a new thread to generate reports periodically
     TweetTimer(1, 'Tweet Timer').start()
